78c185fcf7f3088902e35f34eeba500e272cdb89
Ivan Rojas <184444587+ivanr11@users.noreply.github.com>
2024-10-25 11:01:34 -0400

    Add base api caller, decouple api request and clip download, set types

--- src/api/twitch.ts
+++ src/api/twitch.ts
@@ -1,48 +1,35 @@
 import axios from 'axios';
-import fs from 'fs';
-import { exec } from 'child_process';
 import dotenv from 'dotenv';
-import config from '../../config.json'
+import { GetClipsQueryParams, Clip } from '../types/twitchTypes';
 
 dotenv.config();
 
-// Fetch clips, download and store in 'raw-clips' folder
-export async function getClips() {
+export async function getClips(queryParams: GetClipsQueryParams): Promise<Clip[]> {
+    const baseUrl = 'https://api.twitch.tv/helix';
+    const instance = axios.create({
+        baseURL: baseUrl,
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': process.env.ACCESS_TOKEN,
+            'Client-id': process.env.CLIENT_ID,
+        }
+    });
+
     try {
-        const response = await axios.get(`https://api.twitch.tv/helix/clips`, {
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': process.env.ACCESS_TOKEN,
-                'Client-id': process.env.CLIENT_ID,
-            },
+        const response = await instance.get('/clips', {
             params: {
-                game_id: config.twitch.game_id,
-                started_at: config.twitch.start_date,
-                first: config.twitch.clip_limit,
-            },
+                ...queryParams
+            }
         });
 
-        const clips = response.data.data;
+        const clips: Clip[] = response.data.data;
 
-        const localRawClipsPath = './raw-clips';
-        
-        // Make raw clips directory (if it doesn't already exist)
-        if (!fs.existsSync(localRawClipsPath)) {
-            fs.mkdirSync(localRawClipsPath, { recursive: true });
-            console.log(`Path created at: ${localRawClipsPath}`);
+        if (response.status >= 200 && response.status < 300) {
+            console.log('Request successful:', response.data);
         }
 
-        // Download each clip
-        for (const clip of clips) {
-            exec(`streamlink --output ${localRawClipsPath}/${clip.id}.mp4 ${clip.url} best`)
-            .on('error', (error) => {
-                console.error('Exec error:', error);
-            })
-            .on('close', () => {
-                console.log(`Downloaded clip ${clip.id} to ${localRawClipsPath}/${clip.id}`)
-            });
-        }
+        return clips;
     } catch (error) {
-        console.error('Error fetching clips:', error);
+        throw new Error(`Error fetching clips: ${error}`);
     }
 }
