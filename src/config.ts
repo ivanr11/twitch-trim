9ac8191b036b79604e16e92217a03197c199f8de
Ivan Rojas <184444587+ivanr11@users.noreply.github.com>
2024-10-28 13:00:08 -0400

    add local processed clips path

--- src/config.ts
+++ src/config.ts
@@ -1,17 +1,19 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config();
 
-type Config = {
-    ACCESS_TOKEN?: string;
-    CLIENT_ID?: string;
-    LOCAL_RAW_CLIPS_PATH?: string;
-}
+export type Config = {
+	ACCESS_TOKEN?: string;
+	CLIENT_ID?: string;
+	LOCAL_RAW_CLIPS_PATH?: string;
+	LOCAL_PROCESSED_CLIPS_PATH?: string;
+};
 
 const config: Config = {
-    ACCESS_TOKEN: process.env.ACCESS_TOKEN,
-    CLIENT_ID: process.env.CLIENT_ID,
-    LOCAL_RAW_CLIPS_PATH: process.env.LOCAL_RAW_CLIPS_PATH,
-}
+	ACCESS_TOKEN: process.env.ACCESS_TOKEN,
+	CLIENT_ID: process.env.CLIENT_ID,
+	LOCAL_RAW_CLIPS_PATH: process.env.LOCAL_RAW_CLIPS_PATH,
+	LOCAL_PROCESSED_CLIPS_PATH: process.env.LOCAL_PROCESSED_CLIPS_PATH,
+};
 
 export default config;
