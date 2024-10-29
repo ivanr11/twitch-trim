9a9f67318225a58fe404c83d206c7c667ecb2496
Ivan Rojas <184444587+ivanr11@users.noreply.github.com>
2024-10-29 10:37:32 -0400

    add test project function

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,28 @@
-import express from 'express';
+// import express from 'express';
+import { getClips } from "./api/twitch";
+import { createVideo } from "./api/ffmpeg";
+import { GetClipsQueryParams } from "./types/twitchTypes";
 
-const app = express();
-const port = 3000;
+const queryParams: GetClipsQueryParams = {
+	game_id: "33214",
+	started_at: new Date(Date.now() - 30 * 24 * 60 * 60 * 1000).toISOString(),
+	first: 2,
+};
 
-app.get('/', (req, res) => {
-    res.send('Hello world');
-})
+async function test() {
+	const clips = await getClips(queryParams);
+	createVideo(clips);
+}
 
-app.listen(port, () => {
-    console.log(`App listening on port ${port}`)
-})
+test();
+
+// const app = express();
+// const port = 3000;
+
+// app.get('/', (req, res) => {
+//     res.send('Hello world');
+// })
+
+// app.listen(port, () => {
+//     console.log(`App listening on port ${port}`)
+// })
