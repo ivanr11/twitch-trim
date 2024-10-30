043672657481727440b07a815ccfb6259d21883d
Ivan Rojas <184444587+ivanr11@users.noreply.github.com>
2024-10-30 17:03:54 -0400

    move test project func outside of main file to sandbox file

---
+++ src/scripts/sandbox.ts
@@ -0,0 +1,21 @@
+import { getClips } from "../api/twitch/twitch";
+import { createVideo } from "../api/ffmpeg/ffmpeg";
+import { GetClipsQueryParams } from "../types/twitchTypes";
+import logger from "../logger";
+
+const queryParams: GetClipsQueryParams = {
+	game_id: "33214",
+	started_at: new Date(Date.now() - 30 * 24 * 60 * 60 * 1000).toISOString(),
+	first: 2,
+};
+
+async function testCurrentProject() {
+	try {
+		const clips = await getClips(queryParams);
+		createVideo(clips);
+	} catch (error) {
+		logger.error(error);
+	}
+}
+
+testCurrentProject();
