de195514b114994762f8dfea8f20324a0a38c4f4
Ivan Rojas <184444587+ivanr11@users.noreply.github.com>
2024-11-05 10:47:54 -0500

    add uploadVideo() (untested)

--- src/api/youtube/youtube.ts
+++ src/api/youtube/youtube.ts
@@ -1,13 +1,41 @@
 import { google } from "googleapis";
-import config from "../../config";
+import fs from "fs";
+import path from "path";
+import logger from "../../logger";
+import getOAuth2Client from "./auth";
 
-const oauth2Client = new google.auth.OAuth2(
-	config.YOUTUBE_CLIENT_ID,
-	config.YOUTUBE_CLIENT_SECRET,
-	config.REDIRECT_URL,
-);
+// scope: "https://www.googleapis.com/auth/youtube.upload"
 
-const url = oauth2Client.generateAuthUrl({
-	access_type: "offline",
-	scope: "https://www.googleapis.com/auth/youtube.upload",
-});
+async function uploadVideo(date: string) {
+	try {
+		const oauth2client = await getOAuth2Client();
+
+		const youtube = google.youtube({
+			version: "v3",
+			auth: oauth2client,
+		});
+
+		const videoPath = `output-${date}.mp4`;
+		const videoFilePath = path.resolve(videoPath);
+
+		await youtube.videos.insert({
+			part: ["snippet", "status"],
+			notifySubscribers: false,
+			requestBody: {
+				snippet: {
+					title: "Test video",
+					description: "A test video",
+				},
+				status: {
+					privacyStatus: "private",
+				},
+			},
+			media: {
+				body: fs.createReadStream(videoFilePath),
+				mimeType: "video/mp4",
+			},
+		});
+	} catch (error) {
+		logger.error(`uploadVideo :: ${error}`);
+	}
+}
