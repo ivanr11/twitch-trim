ad647da8e09938671ffa5e5ac9628765fdf97938
Ivan Rojas <184444587+ivanr11@users.noreply.github.com>
2024-11-18 16:17:09 -0500

    delete src backend directory and files for nextjs conversion

--- src/logger.ts
+++
@@ -1,9 +0,0 @@
-import util from "winston";
-
-const logger = util.createLogger({
-	level: "info",
-	format: util.format.simple(),
-	transports: [new util.transports.Console()],
-});
-
-export default logger;
