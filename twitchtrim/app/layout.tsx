dc31ef5570b57cf24a82033199b931bfb6dba1ed
Ivan Rojas <184444587+ivanr11@users.noreply.github.com>
2024-11-18 16:24:11 -0500

    add global ui layout

--- twitchtrim/app/layout.tsx
+++ twitchtrim/app/layout.tsx
@@ -1,3 +1,13 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+	title: "TwitchTrim",
+	description: "Compile and upload Twitch clips",
+};
+
 export default function RootLayout({
 	children,
 }: {
@@ -5,7 +15,7 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
-			<body>{children}</body>
+			<body className={inter.className}>{children}</body>
 		</html>
 	);
 }
